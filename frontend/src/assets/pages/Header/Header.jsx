4f1afc54e6db163a7e8f53d05f44a2ac44d26443
Erich k Boerr <erichkboerr@gmail.com>
2026-06-11 09:17:13 -0300

    feat: add subtitle to header visible from tablet onwards

--- frontend/src/assets/pages/Header/Header.jsx
+++ frontend/src/assets/pages/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "./images/logo_CSJN.png";
+import logo from "./images/Logo-teknik.png";
 import "./css/Header.css";
 import { getRedirectPath } from "../../common/helpers/authHelpers";
 
@@ -7,14 +7,16 @@ const Header = () => {
   return (
     <>
       <nav className="navbar expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to={getRedirectPath()}>
+        <div className="container-fluid d-flex flex-column align-items-center">
+          <Link className="navbar-brand mb-0" to={getRedirectPath()}>
             <img src={logo} alt="" className="rounded mx-auto d-block" />
           </Link>
+          <p className="d-none d-md-block text-center mb-0" style={{ fontSize: "0.85rem", color: "#555" }}>
+            Servicio de mantenimiento Termomecánico y aires acondicionados para la CSJN
+          </p>
         </div>
       </nav>
     </>
   );
 };
-
 export default Header;
